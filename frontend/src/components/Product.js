8dad73b957e83d47d8ca9320d7d2b592bae1d47c
Said Mounaim <saidmounaim00@gmail.com>
2021-12-19 17:25:07 +0100

    Add Rating Component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import Rating from "./Rating";
 
 const Product = ({ product }) => {
   return (
@@ -14,9 +15,7 @@ const Product = ({ product }) => {
           </Card.Title>
         </a>
         <Card.Text as="div">
-          <div className="my-3">
-            {product.rating} from {product.numReviews}
-          </div>
+          <Rating value={product.rating} text={`${product.rating} Reviews`} />
         </Card.Text>
         <Card.Text as="h6">
           <strong>{product.price}$</strong>
---
+++ frontend/src/components/Rating.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+const Rating = ({ value, text }) => {
+  return (
+    <div className="rating">
+      <span>
+        <i
+          class={
+            value >= 1
+              ? `fas fa-star`
+              : value >= 0.5
+              ? `fas fa-star-half-alt`
+              : `far fa-star`
+          }
+        ></i>
+        <i
+          class={
+            value >= 2
+              ? `fas fa-star`
+              : value >= 1.5
+              ? `fas fa-star-half-alt`
+              : `far fa-star`
+          }
+        ></i>
+        <i
+          class={
+            value >= 3
+              ? `fas fa-star`
+              : value >= 2.5
+              ? `fas fa-star-half-alt`
+              : `far fa-star`
+          }
+        ></i>
+        <i
+          class={
+            value >= 4
+              ? `fas fa-star`
+              : value >= 3.5
+              ? `fas fa-star-half-alt`
+              : `far fa-star`
+          }
+        ></i>
+        <i
+          class={
+            value >= 5
+              ? `fas fa-star`
+              : value >= 4.5
+              ? `fas fa-star-half-alt`
+              : `far fa-star`
+          }
+        ></i>
+      </span>
+      <span> {text && text}</span>
+    </div>
+  );
+};
+
+export default Rating;
