46e194574c26a33aa254d8733841639cb4d4c616
Said Mounaim <saidmounaim00@gmail.com>
2021-12-20 21:28:40 +0100

    Connect to database with mongoose

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Database is connected");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./config/db.js";
 
 const app = express();
 
 dotenv.config();
+
+//Connect Database
+connectDB();
 
 app.use(cors());
 app.use(express.json());
