e753466b880cd40da55ab7cd460f74f502626f10
Said Mounaim <saidmounaim00@gmail.com>
2021-12-20 20:49:29 +0100

    Setup Backend With Express

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+const app = express();
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+// Default Route
+app.get("/", (req, res) => {
+  res.status(201).json({ success: true, message: "Welcome Cloth Shop APP" });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server is running on Port ${PORT}`));
