84a95de22bfaa7ca64500829fedea497f6a2454a
Said Mounaim <saidmounaim00@gmail.com>
2021-12-29 23:06:48 +0100

    Add CheckoutSteps Component

---
+++ frontend/src/components/CheckoutSteps.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
+const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
+  return (
+    <Nav className="justify-content-center mb-4">
+      <Nav.Item>
+        {step1 ? (
+          <LinkContainer to="/login">
+            <Nav.Link>Sign in</Nav.Link>
+          </LinkContainer>
+        ) : (
+          <Nav.Link disabled>Sign in</Nav.Link>
+        )}
+      </Nav.Item>
+      <Nav.Item>
+        {step2 ? (
+          <LinkContainer to="/shipping">
+            <Nav.Link>Shipping</Nav.Link>
+          </LinkContainer>
+        ) : (
+          <Nav.Link disabled>Shipping</Nav.Link>
+        )}
+      </Nav.Item>
+      <Nav.Item>
+        {step3 ? (
+          <LinkContainer to="/payment">
+            <Nav.Link>Payment</Nav.Link>
+          </LinkContainer>
+        ) : (
+          <Nav.Link disabled>Payment</Nav.Link>
+        )}
+      </Nav.Item>
+      <Nav.Item>
+        {step4 ? (
+          <LinkContainer to="/placeorder">
+            <Nav.Link>Place order</Nav.Link>
+          </LinkContainer>
+        ) : (
+          <Nav.Link disabled>Place order</Nav.Link>
+        )}
+      </Nav.Item>
+    </Nav>
+  );
+};
+
+export default CheckoutSteps;
--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -4,6 +4,7 @@ import { Button, Form } from "react-bootstrap";
 import FormContainer from "../components/FormContainer";
 import { useSelector, useDispatch } from "react-redux";
 import { saveShippingAddress } from "../redux/actions/cartActions";
+import CheckoutSteps from "../components/CheckoutSteps";
 
 const ShippingScreen = () => {
   let navigate = useNavigate();
@@ -25,6 +26,7 @@ const ShippingScreen = () => {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={handleShippingAddress}>
         <Form.Group className="py-2" controlId="address">
