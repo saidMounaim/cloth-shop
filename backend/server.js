37d59d6434efa6dfc40089d3683ef027d31f4a80
Said Mounaim <saidmounaim00@gmail.com>
2022-01-22 12:54:29 +0100

    Fix Config Production

--- backend/server.js
+++ backend/server.js
@@ -46,9 +46,9 @@ app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
 } else {
   // Default Route
   app.get("/api", (req, res) => {
