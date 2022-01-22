bb2ef31226c69bec7f9e451b9fa3baa6d779adee
Said Mounaim <saidmounaim00@gmail.com>
2022-01-22 12:51:20 +0100

    fix server js file

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
-  app.use("*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 } else {
